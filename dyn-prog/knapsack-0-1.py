9476eae67c1c88ab7abe79c9970c5e042d3f0e2f
vchapple17 <vchapple17@gmail.com>
2017-11-03 15:05:02 -0500

    two sets of data

--- dyn-prog/knapsack-0-1.py
+++ dyn-prog/knapsack-0-1.py
@@ -5,9 +5,23 @@
 
 import string
 
-# GLOBAL CONSTANTS
-MAX_W = 5
+maxW = 0
+items = []
 
+maxWeights = [
+    5,  # Set 0
+    165 # Set 1
+]
+
+weights = [
+[ 2, 3, 4, 5 ], # Set 0
+[ 23, 31, 29, 44, 53, 38, 63, 85, 89, 82 ] # Set 1
+]
+
+profits = [
+[ 3, 4, 5, 6 ], # Set 0
+[ 92, 57, 49, 68, 60, 43, 67, 84, 87, 72 ] # Set 1
+]
 # CLASSES
 class Item:
     def __init__(self, weight, benefit):
@@ -19,17 +33,41 @@ class Item:
         return self.w
 
 # FUNCTIONS
+
+def setMax( m ):
+    global maxW 
+    maxW = m
+
+def setItems( i ):
+    global items 
+    items = i 
+
 # initItems( )
 # PRE: None
 # POST: returns list of objects of type Item
-def initItems( ):
+def initItems( setNum ):
     items = []
     items.append( None ) 
-    items.append( Item( 2, 3 ) )
-    items.append( Item( 3, 4 ) )
-    items.append( Item( 4, 5 ) )
-    items.append( Item( 5, 6 ) ) 
-    return items
+
+    #if setNum == 0:
+        #items.append( Item( 2, 3 ) )
+        #items.append( Item( 3, 4 ) )
+        #items.append( Item( 4, 5 ) )
+        #items.append( Item( 5, 6 ) ) 
+    wArray = weights[ setNum ]
+    pArray = profits[ setNum ]
+
+    for i in range( len(wArray) ):
+        items.append( Item( wArray[i], pArray[i] ) )
+    setItems(items)
+
+def initMax( setNum ):
+    setMax( maxWeights[ setNum ] )
+
+def initScene( setNum):
+    initMax( setNum )
+    initItems( setNum)
+
 
 def printArray( array, r, c ):
     # Heading
@@ -54,12 +92,16 @@ def printArray( array, r, c ):
 # PRE: None
 # POST: Print max benefit of having Items and MAX_W
 def main():
-    items = initItems()
+
+    setNum = 1
+    initScene( setNum )
+ 
     n = len(items) - 1 # index 1 based
 
     numRows = n + 1
-    numCols = MAX_W + 1
+    numCols = maxW + 1
     benefit = [[ 0 for j in range( numCols ) ] for i in range( numRows ) ]
+
 
     for i in range( 1, numRows ):
         for w in range( 1, numCols ):
@@ -74,7 +116,7 @@ def main():
 #    printArray( benefit , numRows , numCols )
 
     # Print Max benefit
-    print( benefit[ n ][ MAX_W ] )
+    print( benefit[ n ][ maxW ] )
 
 
 main()
