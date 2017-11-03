4b651b6ea7747071912dc7358833e6aac583ff90
vchapple17 <vchapple17@gmail.com>
2017-11-03 14:23:27 -0500

    knapsack v1

--- dyn-prog/knapsack-0-1.py
+++ dyn-prog/knapsack-0-1.py
@@ -6,7 +6,7 @@
 import string
 
 # GLOBAL CONSTANTS
-MAX_W = 20
+MAX_W = 5
 
 # CLASSES
 class Item:
@@ -24,6 +24,7 @@ class Item:
 # POST: returns list of objects of type Item
 def initItems( ):
     items = []
+    items.append( None ) 
     items.append( Item( 2, 3 ) )
     items.append( Item( 3, 4 ) )
     items.append( Item( 4, 5 ) )
@@ -54,13 +55,26 @@ def printArray( array, r, c ):
 # POST: Print max benefit of having Items and MAX_W
 def main():
     items = initItems()
-    n = len(items)
+    n = len(items) - 1 # index 1 based
 
-    numCols = 5
-    numRows = 10 
-    array = [[ 0 for j in range( numCols + 1) ] for i in range( numRows ) ]
-    printArray( array, numRows, numCols + 1 )
+    numRows = n + 1
+    numCols = MAX_W + 1
+    benefit = [[ 0 for j in range( numCols ) ] for i in range( numRows ) ]
 
+    for i in range( 1, numRows ):
+        for w in range( 1, numCols ):
+            if items[i].getW() > w:
+                benefit[i][w] = benefit[ i-1][ w ]
+            else:
+                skipItem = benefit[ i-1 ][ w ]
+                addItem = benefit[ i-1 ][ w - items[i].getW() ] + items[i].getB()                
+                benefit[i][w] = max( skipItem, addItem ) 
+
+    # Print Array
+#    printArray( benefit , numRows , numCols )
+
+    # Print Max benefit
+    print( benefit[ n ][ MAX_W ] )
 
 
 main()
